389e5d5283f325c0c69acfcd8cb82e3ec36c9503
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-11-08 04:44:40 -0800

    Added Config.GetSetting.

--- src/Core/Config.cs
+++ src/Core/Config.cs
@@ -9,10 +9,8 @@ namespace NetStandardTypes
 {
     public static class Config
     {
-        public static string AzureSearchKey { get; } = CloudConfigurationManager.GetSetting("NETSTANDARDTYPES_SEARCHKEY") ??
-            Environment.GetEnvironmentVariable("NETSTANDARDTYPES_SEARCHKEY");
-        private static string AzureStorageKey { get; } = CloudConfigurationManager.GetSetting("NETSTANDARDTYPES_STORAGEKEY") ??
-            Environment.GetEnvironmentVariable("NETSTANDARDTYPES_STORAGEKEY");
+        private static string AzureSearchKey { get; } = GetSetting("NETSTANDARDTYPES_SEARCHKEY");
+        private static string AzureStorageKey { get; } = GetSetting("NETSTANDARDTYPES_STORAGEKEY");
 
         private static Uri SearchUri { get; } = new Uri("https://netstandardtypes.search.windows.net");
         private static Uri QueueUri { get; } = new Uri("https://netstandardtypes.queue.core.windows.net/");
@@ -23,5 +21,7 @@ namespace NetStandardTypes
         public static SearchServiceClient CreateSearchServiceClient() => new SearchServiceClient(SearchUri, SearchCredentials, HttpClientHandler);
 
         public static CloudQueueClient CreateCloudQueueClient() => new CloudQueueClient(QueueUri, StorageCredentials);
+
+        private static string GetSetting(string name) => CloudConfigurationManager.GetSetting(name) ?? Environment.GetEnvironmentVariable(name);
     }
 }
