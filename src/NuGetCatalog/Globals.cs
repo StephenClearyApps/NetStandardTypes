288da79d760dfa9052f4dda6bdc24e4fe658c38a
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-11-08 12:13:50 -0800

    Remove HTTP logging.

--- src/NuGetCatalog/Globals.cs
+++ src/NuGetCatalog/Globals.cs
@@ -18,7 +18,7 @@ namespace NuGetCatalog
 
         internal static async Task<JObject> GetJsonAsync(this HttpClient client, string url)
         {
-            Console.WriteLine("GET " + url);
+            Trace.WriteLine("GET " + url);
             var json = await Policy.Handle<Exception>()
                 .WaitAndRetryAsync(20, count => TimeSpan.FromSeconds(count))
                 .ExecuteAsync(() => client.GetStringAsync(url)).ConfigureAwait(false);
